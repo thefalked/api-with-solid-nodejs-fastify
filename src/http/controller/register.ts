a74a507ed2bd714407d3d55a6be910802bbb7fbb
Giuliano Crivelli <giuliano.crivelli@byside.com>
2023-03-19 19:35:14 -0400

    feat: creating controller and routers file

---
+++ src/http/controller/register.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
