be3cda7f9f15ea31c7a8737ca131edd7decc272e
Giuliano Crivelli <giuliano.crivelli@byside.com>
2023-04-02 01:02:25 -0400

    test: creating a different schema when using e2e test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,40 @@
+import 'dotenv/config'
+import crypto from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const generateDatabaseURL = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+const prisma = new PrismaClient()
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Executou')
+    const schema = crypto.randomUUID()
+    const databseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {},
+      async teardown() {
+        await prisma.$queryRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
+      },
     }
   },
 }
