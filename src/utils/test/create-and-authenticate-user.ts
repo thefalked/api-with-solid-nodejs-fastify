efda1b995ce6338734fcd7f4d76f5def8f43edee
Giuliano Crivelli <giuliano.crivelli@byside.com>
2023-04-03 01:34:52 -0400

    test: adding option on utils function to create user has admin

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,5 +1,8 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
+
+import { prisma } from '@/lib/prisma'
 
 interface CreateAndAuthenticateUserResponse {
   token: string
@@ -7,11 +10,15 @@ interface CreateAndAuthenticateUserResponse {
 
 export const createAndAuthenticateUser = async (
   app: FastifyInstance,
+  isAdmin = false,
 ): Promise<CreateAndAuthenticateUserResponse> => {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john.doe@gmail.com',
-    password: '123456',
+  await prisma.user.create({
+    data: {
+      name: 'John.doe',
+      email: 'john.doe@gmail.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
